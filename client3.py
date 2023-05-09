34a980f6b5b679cf5c4a6926dad557baa0560bdb
Myles Magee <myles.ivworldwide@gmail.com>
2023-05-09 11:28:06 -0700

    Updated the client3.py and client_test.py to complete assignment

--- client3.py
+++ client3.py
@@ -21,6 +21,7 @@
 import json
 import random
 import urllib.request
+from fractions import Fraction
 
 # Server API URLs
 QUERY = "http://localhost:8080/query?id={}"
@@ -35,14 +36,21 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = round(((bid_price + ask_price) / 2), 2)
+
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    price1 = Fraction.from_float(price_a).limit_denominator()
+    price2 = Fraction.from_float(price_b).limit_denominator()
+
+    ratio = price1 / price2
+
+    return f"{ratio.numerator}:{ratio.denominator}"
+
 
 
 # Main
@@ -52,8 +60,10 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
